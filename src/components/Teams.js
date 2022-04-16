66d85b1070e8868236426bb2c67c923115190595
meinhoonharsh <harshprogrammer782@gmail.com>
2022-04-16 13:41:26 +0530

    Mapped Teams Data in Teams Component

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,6 +1,7 @@
 import React from 'react'
-
+import TeamMembers from '../teams.json'
 export default function Teams() {
+    console.log(TeamMembers)
     return (
         <>
 
@@ -20,181 +21,54 @@ export default function Teams() {
                         </div>
                         <div className="swiper-container mid-slider-simple items">
                             <div className="swiper-wrapper">
-                                <div className="swiper-slide slide-center text-center item">
-                                    <div className="row card">
-                                        <div className="col-12">
-                                            <img
-                                                src="assets/images/team-1.jpg"
-                                                alt="Adams Baker"
-                                                className="person"
-                                            />
-                                            <h4>Adams Baker</h4>
-                                            <p>CEO / CO-FOUNDER</p>
-                                            <ul className="navbar-nav social share-list ml-auto">
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-instagram" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-facebook" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-linkedin" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-twitter" />
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="swiper-slide slide-center text-center item">
-                                    <div className="row card">
-                                        <div className="col-12">
-                                            <img
-                                                src="assets/images/team-2.jpg"
-                                                alt="Mary Evans"
-                                                className="person"
-                                            />
-                                            <h4>Mary Evans</h4>
-                                            <p>CONTROLLING / FOUNDER</p>
-                                            <ul className="navbar-nav social share-list ml-auto">
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-instagram" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-facebook" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-linkedin" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-twitter" />
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="swiper-slide slide-center text-center item">
-                                    <div className="row card">
-                                        <div className="col-12">
-                                            <img
-                                                src="assets/images/team-3.jpg"
-                                                alt="Sarah Lopez"
-                                                className="person"
-                                            />
-                                            <h4>Sarah Lopez</h4>
-                                            <p>HUMAN RESOURCES</p>
-                                            <ul className="navbar-nav social share-list ml-auto">
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-instagram" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-facebook" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-linkedin" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-twitter" />
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="swiper-slide slide-center text-center item">
-                                    <div className="row card">
-                                        <div className="col-12">
-                                            <img
-                                                src="assets/images/team-4.jpg"
-                                                alt="Joseph Hills"
-                                                className="person"
-                                            />
-                                            <h4>Joseph Hills</h4>
-                                            <p>CFO / CHIEF FINANCIAL</p>
-                                            <ul className="navbar-nav social share-list ml-auto">
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-instagram" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-facebook" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-linkedin" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-twitter" />
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="swiper-slide slide-center text-center item">
-                                    <div className="row card">
-                                        <div className="col-12">
-                                            <img
-                                                src="assets/images/team-5.jpg"
-                                                alt="Karen Usman"
-                                                className="person"
-                                            />
-                                            <h4>Karen Usman</h4>
-                                            <p>CTO / CHIEF MARKETING</p>
-                                            <ul className="navbar-nav social share-list ml-auto">
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-instagram" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-facebook" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-linkedin" />
-                                                    </a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a href="#" className="nav-link">
-                                                        <i className="icon-social-twitter" />
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
+
+
+
+                                {
+                                    TeamMembers.map((member, index) => {
+                                        return (
+
+                                            <div className="swiper-slide slide-center text-center item" >
+                                                <div className="row card">
+                                                    <div className="col-12">
+                                                        <img
+                                                            src={member.image}
+                                                            alt="Adams Baker"
+                                                            className="person"
+                                                        />
+                                                        <h4>{member.name}</h4>
+                                                        <p>{member.position}</p>
+                                                        <ul className="navbar-nav social share-list ml-auto">
+                                                            <li className="nav-item">
+                                                                <a href="#" className="nav-link">
+                                                                    <i className="icon-social-instagram" />
+                                                                </a>
+                                                            </li>
+                                                            <li className="nav-item">
+                                                                <a href="#" className="nav-link">
+                                                                    <i className="icon-social-facebook" />
+                                                                </a>
+                                                            </li>
+                                                            <li className="nav-item">
+                                                                <a href="#" className="nav-link">
+                                                                    <i className="icon-social-linkedin" />
+                                                                </a>
+                                                            </li>
+                                                            <li className="nav-item">
+                                                                <a href="#" className="nav-link">
+                                                                    <i className="icon-social-twitter" />
+                                                                </a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+
+                                }
+
+
                             </div>
                             <div className="swiper-pagination" />
                         </div>
